48d0e8caec40b9c7af22f7b953b12fd5c4037709
YODC <38653678+DCJLN@users.noreply.github.com>
2021-05-24 20:27:53 +0200

    v1.2

--- file_organizer.py
+++ file_organizer.py
@@ -6,7 +6,7 @@ import sys
 def main():
 
     if (len(sys.argv) != 2):
-        print("Please, enter a valid number of argument (should be one, the top folder path).")
+        print('Please, enter a valid number of argument (should be one, the top folder path).')
         sys.exit()
     else:
         top_dir = sys.argv[1]
@@ -14,10 +14,10 @@ def main():
 
     doc_types = ('.doc', '.docx', '.txt', '.pdf', '.xls', '.xlsm', '.xlsx', '.ppt')
     img_types = ('.jpg', '.jpeg', '.png', '.svg', '.gif')
-    software_types = ('.exe', '.pkg', '.dmg')
+    software_types = ('.exe', '.pkg', '.dmg', 'msi')
     
     try:
-        for _, dirs, files in os.walk(".", topdown=True):
+        for _, dirs, files in os.walk('.', topdown=True):
             for file in files:
                 if file.endswith(doc_types):
                     if not 'Documents' in dirs:
@@ -37,12 +37,11 @@ def main():
                         dirs.append('Applications')
                     shutil.move(file, 'Applications')
 
-                #else:
-                #    print('Others')
-                    #if not 'Others' in dirs:
-                    #    os.mkdir(os.path.join(start_dir,'Others'))
-                    #    dirs.append('Others')
-                    #shutil.move(file, 'Others')
+                else:
+                    if not 'Others' in dirs:
+                        os.mkdir('Others')
+                        dirs.append('Others')
+                    shutil.move(file, 'Others')
             break
     except Exception as e:
         print(e)
